48643950f2558662b7b3af9c3bdbd7d026edb81f
rvchauhan <ravi.chauhan@successive.tech>
2020-03-18 18:47:30 +0530

    refactor: merge conflicts app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
+
 // import { ThemeProvider, Typography } from '@material-ui/core';
 // import Theme from './theme';
 // import ChildrenDemo from './pages/TextFieldDemo/ChildrenDemo';
