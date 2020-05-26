3f90c3b2304d19468d1f5f3566dbce9793ddb799
rvchauhan <ravi.chauhan@successive.tech>
2020-05-26 17:54:43 +0530

    refactor: formDialog.js, Trainee.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-
-// import { ThemeProvider, Typography } from '@material-ui/core';
-// import Theme from './theme';
-// import ChildrenDemo from './pages/TextFieldDemo/ChildrenDemo';
 import Trainee from './pages/Trainee/Trainee';
 
 function App() {
   return (
     <div>
-      {/* <ThemeProvider theme={Theme}>
-        <Typography>
-        </Typography>
-      </ThemeProvider> */}
       <Trainee />
     </div>
   );
--- src/pages/Trainee/components/FormDialog/FormDialog.jsx
+++ src/pages/Trainee/components/FormDialog/FormDialog.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import * as yup from 'yup';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -14,14 +13,7 @@ import PropTypes from 'prop-types';
 import EmailIcon from '@material-ui/icons/Email';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 import Grid from '@material-ui/core/Grid';
-
-const schema = yup.object().shape({
-  name: yup.string().required('Name is required').min(3),
-  email: yup.string().email().required('Email is required'),
-  password: yup.string().required('Password is required').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9]{8,}$/,
-    'Must contain 8 characters at least one uppercase one lowercase and one number'),
-  confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required('Confirm password is required'),
-});
+import schema from './helper';
 
 const useStyles = () => ({
   root: {
